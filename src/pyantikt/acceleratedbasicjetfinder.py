f4218bbb861a61193a3ab187c03e06cd707ff970
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2023-04-27 16:43:56 +0200

    Switch away from masked arrays to manual mask
    Masked arrays don't work very well as when jitted the wrong (unmasked)
    version of argmin() is called, so previously merged pseudojets can be
    mistakenly reconsidered.
    
    Solution is to switch instead to normal numpy arrays, but also have a
    mask array that can be used as needed to hide merged jet slots.
    
    Added the antikt_distance array as we do need both - the nearest
    neighbour is decided geomertically (dist), but the comparison metric is
    decided also using the momentum of the jets (akt_dist).
    
    Fixed a few bugs in which jets to updated after a merger and added
    a missing abs() in the phi distance calculation.

--- src/pyantikt/acceleratedbasicjetfinder.py
+++ src/pyantikt/acceleratedbasicjetfinder.py
@@ -16,7 +16,6 @@ logger = logging.getLogger("jetfinder")
 
 from numba import njit
 
-
 Invalid = -3
 NonexistentParent = -2
 BeamJet = -1
@@ -47,52 +46,62 @@ def antikt_distance(jetAdist: float, jetAipt2: float, jetBipt2:float):
         inv_pt2 = jetAipt2
     return jetAdist * inv_pt2
 
-# Oh, jitting this function causes it to *fail*, not using masking properly!
-#@njit
-def find_closest_jets(dist: npt.ArrayLike, nn: npt.ArrayLike):
+@njit
+def find_closest_jets(akt_dist:npt.ArrayLike, nn:npt.ArrayLike):
     '''Look over active jets and find the closest'''
-    closest = dist.argmin()
-    return dist[closest], closest
+    closest = akt_dist.argmin()
+    return akt_dist[closest], closest
 
 
 @njit
 def scan_for_all_nearest_neighbours(phi: npt.ArrayLike, rap: npt.ArrayLike, inv_pt2: npt.ArrayLike,
-                                    dist: npt.ArrayLike, nn: npt.ArrayLike, mask:npt.ArrayLike, R2:float):
+                                    dist: npt.ArrayLike, akt_dist: npt.ArrayLike,
+                                    nn: npt.ArrayLike, mask:npt.ArrayLike, R2:float):
     '''Do a full scan for nearest (geometrical) neighbours'''
     for ijet in range(phi.size):
-        if bool(phi[ijet]) == False:
+        if mask[ijet]:
             continue
-        _dphi = np.pi - np.abs(np.pi - (phi - phi[ijet]))
+        _dphi = np.pi - np.abs(np.pi - np.abs(phi - phi[ijet]))
         _drap = rap - rap[ijet]
         _dist = _dphi*_dphi + _drap*_drap
-        _dist[ijet] = R2
-        iclosejet = _dist[~mask].argmin()
+        _dist[ijet] = R2 # Avoid measuring the distance 0 to myself!
+        _dist[mask] = 1e20 # Don't consider any masked jets
+        iclosejet = _dist.argmin()
         dist[ijet] = _dist[iclosejet]
         if iclosejet == ijet:
             nn[ijet] = -1
-            dist[ijet] *= inv_pt2[ijet]
+            akt_dist[ijet] = dist[ijet] * inv_pt2[ijet]
         else:
             nn[ijet] = iclosejet
-            dist[ijet] *= inv_pt2[ijet] if inv_pt2[ijet] < inv_pt2[iclosejet] else inv_pt2[iclosejet]
+            akt_dist[ijet] = dist[ijet] * (inv_pt2[ijet] if inv_pt2[ijet] < inv_pt2[iclosejet] else inv_pt2[iclosejet])
 
 @njit
 def scan_for_my_nearest_neighbours(ijet:int, phi: npt.ArrayLike, 
                                    rap:npt.ArrayLike, inv_pt2:npt.ArrayLike, 
-                                   dist:npt.ArrayLike, nn:npt.ArrayLike, 
+                                   dist:npt.ArrayLike, akt_dist:npt.ArrayLike, nn:npt.ArrayLike, 
                                    mask:npt.ArrayLike, R2: float):
     '''Retest all other jets against the target jet'''
-    _dphi = np.pi - np.abs(np.pi - (phi - phi[ijet]))
+    nn[ijet] = -1
+    dist[ijet] = R2
+    _dphi = np.pi - np.abs(np.pi - np.abs(phi - phi[ijet]))
     _drap = rap - rap[ijet]
     _dist = _dphi*_dphi + _drap*_drap
-    _dist[ijet] = R2
-    iclosejet = _dist[~mask].argmin()
+    _dist[ijet] = R2 # Avoid measuring the distance 0 to myself!
+    _dist[mask] = 1e20 # Don't consider any masked jets
+    iclosejet = _dist.argmin()
     dist[ijet] = _dist[iclosejet]
     if iclosejet == ijet:
         nn[ijet] = -1
-        dist[ijet] *= inv_pt2[ijet]
+        akt_dist[ijet] = dist[ijet] * inv_pt2[ijet]
     else:
         nn[ijet] = iclosejet
-        dist[ijet] *= inv_pt2[ijet] if inv_pt2[ijet] < inv_pt2[iclosejet] else inv_pt2[iclosejet]
+        akt_dist[ijet] = dist[ijet] * (inv_pt2[ijet] if inv_pt2[ijet] < inv_pt2[iclosejet] else inv_pt2[iclosejet])
+        # As this function is called on new PseudoJets it's possible
+        # that we are now the NN of our NN
+        if dist[iclosejet] > dist[ijet]:
+            dist[iclosejet] = dist[ijet]
+            nn[iclosejet] = ijet
+            akt_dist[iclosejet] = dist[iclosejet] * (inv_pt2[ijet] if inv_pt2[ijet] < inv_pt2[iclosejet] else inv_pt2[iclosejet])
  
 
 def test_for_nearest_neighbour(jetA: PseudoJet, jetB: PseudoJet):
@@ -108,6 +117,17 @@ def test_for_nearest_neighbour(jetA: PseudoJet, jetB: PseudoJet):
         jetB.info.nn_dist = dist
         jetB.info.nn = jetA.info.id
         jetB.info.akt_dist = antikt_distance(jetB.info.nn_dist, jetB.inv_pt2, jetA.inv_pt2)
+
+
+def compare_status(working:NPPseudoJets, test:NPPseudoJets):
+    '''Test two different copies of numpy pseudojet containers that should be equal'''
+    dist_diff = working.akt_dist!=test.akt_dist
+    idist_diff = np.where(dist_diff)
+    if len(idist_diff[0]) > 0:
+        print(f"Differences found after full scan of NNs: {idist_diff[0]}")
+        for ijet in idist_diff[0]:
+            print(f"{ijet}\nW: {working.print_jet(ijet)}\nT: {test.print_jet(ijet)}")
+        raise RuntimeError("Jet sets are not the same and they should be!")
 
 
 def add_step_to_history(history: list[HistoryElement], jets: list[PseudoJet], 
@@ -182,25 +202,24 @@ def basicjetfinder(initial_particles: list[PseudoJet], Rparam: float=0.4, ptmin:
     # for fast calculations
     npjets = NPPseudoJets(len(jets) * 2)
     npjets.set_jets(jets)
-    logger.debug(npjets)
 
     # Setup the nearest neighbours, which is an expensive
     # initial operation (N^2 scaling here)
     scan_for_all_nearest_neighbours(npjets.phi, npjets.rap, npjets.inv_pt2, 
-                                    npjets.dist, npjets.nn, npjets.dist.mask, R2)
+                                    npjets.dist, npjets.akt_dist, npjets.nn, npjets.mask, R2)
     logger.debug(npjets)
 
     # Each iteration we either merge two jets to one, or we
     # finalise a jet. Thus it takes a number of iterations
     # equal to the number of jets to finish
     for iteration in range(len(initial_particles)):
-        distance, ijetA = find_closest_jets(npjets.dist, npjets.nn)
+        distance, ijetA = find_closest_jets(npjets.akt_dist, npjets.nn)
         ijetB = npjets.nn[ijetA]
         # Add normalisation for real distance
         distance *= invR2
         logger.debug(f"Iteration {iteration+1}: {distance} for jet {ijetA} and jet {ijetB}")
 
-        if (ijetB != -1):
+        if (ijetB >= 0):
             if ijetB < ijetA:
                 ijetA, ijetB = ijetB, ijetA
 
@@ -211,7 +230,6 @@ def basicjetfinder(initial_particles: list[PseudoJet], Rparam: float=0.4, ptmin:
             merged_jet.info = BasicJetInfo(id = len(jets), nn_dist=R2)
             jets.append(merged_jet)
             inewjet = npjets.insert_jet(merged_jet)
-            logger.debug(npjets)
             add_step_to_history(history=history, jets=jets, 
                                 parent1=jets[ijetA].cluster_hist_index,
                                 parent2=jets[ijetB].cluster_hist_index,
@@ -219,33 +237,32 @@ def basicjetfinder(initial_particles: list[PseudoJet], Rparam: float=0.4, ptmin:
             
             # Get the NNs for the merged pseudojet
             scan_for_my_nearest_neighbours(inewjet, npjets.phi, npjets.rap, npjets.inv_pt2, 
-                                           npjets.dist, npjets.nn, npjets.dist.mask, R2)
-            logger.debug(npjets)
+                                           npjets.dist, npjets.akt_dist, npjets.nn, npjets.mask, R2)
         else:
             # Beamjet
             npjets.mask_slot(ijetA)
-            add_step_to_history(history=history, jets=jets, parent1=jetA.cluster_hist_index, parent2=BeamJet, 
+            add_step_to_history(history=history, jets=jets, parent1=jets[ijetA].cluster_hist_index, 
+                                parent2=BeamJet, 
                                 jetp_index=Invalid, distance=distance)
 
-        # Now need to update nearest distances
-        # for ijet, nn in np.ndenumerate(npjets.nn):
-        #     print(ijet, nn)
-        exit(0)
+        # Now need to update nearest distances, when pseudojets are unmasked and
+        # had either jetA or jetB as their nearest neighbour
+        if ijetB != -1:
+            jets_to_update = np.logical_and(~npjets.mask, np.logical_or(npjets.nn == ijetA , npjets.nn == ijetB))
+        else:
+            jets_to_update = np.logical_and(~npjets.mask, npjets.nn == ijetA)
+        ijets_to_update = np.where(jets_to_update)
 
-        # for jet in jets:
-        #     if jet.info.active:
-        #         # If a jet had jetA or JetB as NN, this is invalid now - rescan the active jets
-        #         # If a jet had None as NN, test against the new jet
-        #         # If a jet had an NN, but not A or B, test against the new jet
-        #         if jet.info.nn == jetA.info.id:
-        #             scan_for_my_nearest_neighbours(jet, jets, R2)
-        #         elif jetB and jet.info.nn == jetB.info.id:
-        #             scan_for_my_nearest_neighbours(jet, jets, R2)
-        #         elif jetB:
-        #             test_for_nearest_neighbour(jet, jets[-1])
+        # Doable without actually needing a loop?
+        for ijet_to_update in ijets_to_update[0]:
+            scan_for_my_nearest_neighbours(ijet_to_update, npjets.phi, npjets.rap, npjets.inv_pt2, 
+                                npjets.dist, npjets.akt_dist, npjets.nn, npjets.mask, R2)
 
-    for jet in jets:
-        if jet.info.active:
-            print(f"Jet {jet.info.id} is still active")
+        # Useful to check that we have done all updates correctly (only for debug!)
+        if logger.level == logging.DEBUG:
+            npjets_copy = deepcopy(npjets)
+            scan_for_all_nearest_neighbours(npjets_copy.phi, npjets_copy.rap, npjets_copy.inv_pt2, 
+                                npjets_copy.dist, npjets.akt_dist, npjets_copy.nn, npjets_copy.mask, R2)
+            compare_status(npjets, npjets_copy)
 
     return inclusive_jets(jets, history, ptmin=ptmin)
--- src/pyantikt/nppseudojet.py
+++ src/pyantikt/nppseudojet.py
@@ -12,11 +12,13 @@ class NPPseudoJets:
         '''Setup blank arrays that will be filled later'''
         self.size = size
         self.next_slot = 0
-        self.phi = ma.masked_array(np.zeros(size, dtype=float), np.zeros(size))
-        self.rap = ma.masked_array(np.zeros(size, dtype=float), np.zeros(size))
-        self.inv_pt2 = ma.masked_array(np.zeros(size, dtype=float), np.zeros(size))
-        self.dist = ma.masked_array(np.zeros(size, dtype=float), np.zeros(size))
-        self.nn = ma.masked_array(np.zeros(size, dtype=int), np.zeros(size)) # Index of my nearest neighbour
+        self.phi = np.zeros(size, dtype=float)
+        self.rap = np.zeros(size, dtype=float)
+        self.inv_pt2 = np.zeros(size, dtype=float)
+        self.dist = np.zeros(size, dtype=float)
+        self.akt_dist = np.zeros(size, dtype=float)
+        self.nn = np.zeros(size, dtype=int) # Index of my nearest neighbour
+        self.mask = np.ones(size, dtype=bool)
     
     def set_jets(self, jets:list[PseudoJet]):
         if len(jets) > self.phi.size:
@@ -26,19 +28,22 @@ class NPPseudoJets:
             self.rap[ijet] = jet.rap
             self.inv_pt2[ijet] = jet.inv_pt2
             self.nn[ijet] = -1
-            self.dist[ijet] = 1e9
-        for ijet in range(len(jets), self.phi.size):
-            self.phi[ijet] = self.rap[ijet] = self.inv_pt2[ijet] = self.dist[ijet] = self.nn[ijet] = ma.masked
+            self.dist[ijet] = self.akt_dist[ijet] = 1e20
+            self.mask[ijet] = False
         self.next_slot = len(jets)
+        self.dist[len(jets):] = self.akt_dist[len(jets):] = 1e20
 
     def __str__(self):
         _string = ""
         for ijet in range(self.phi.size):
-            _string += f"{ijet} - {self.phi[ijet]} {self.rap[ijet]} {self.inv_pt2[ijet]} {self.dist[ijet]} {self.nn[ijet]}\n"
+            _string += (f"{ijet} - {self.phi[ijet]} {self.rap[ijet]} {self.inv_pt2[ijet]} {self.dist[ijet]} "
+            f"{self.akt_dist[ijet]} {self.nn[ijet]} "
+            f"(mask: {self.mask[ijet]})\n")
         return _string
     
     def mask_slot(self, ijet: int):
-        self.phi[ijet] = self.rap[ijet] = self.inv_pt2[ijet] = self.dist[ijet] = self.nn[ijet] = ma.masked
+        self.mask[ijet] = True
+        self.dist[ijet] = self.akt_dist[ijet] = 1e20
 
     def insert_jet(self, jet: PseudoJet, slot:int = -1) -> int:
         '''Add a new pseudojet into the numpy structures'''
@@ -47,12 +52,17 @@ class NPPseudoJets:
                 raise RuntimeError(f"Numpy jet containers are full (size {self.size})")
             slot = self.next_slot
             self.next_slot += 1
-        # Setting values in these arrays also makes them valid (i.e., not masked)
         self.phi[slot] = jet.phi
         self.rap[slot] = jet.rap
         self.inv_pt2[slot] = jet.inv_pt2
         self.nn[slot] = -1
-        self.dist[slot] = 1e9 # Necessary?
+        self.dist[slot] = self.akt_dist[slot] = 1e20 # Necessary?
+        self.mask[slot] = False
 
         return slot
+    
+    def print_jet(self, ijet:int):
+        return (f"{ijet} - {self.phi[ijet]} {self.rap[ijet]} {self.inv_pt2[ijet]} "
+        f"{self.dist[ijet]} {self.akt_dist[ijet]} {self.nn[ijet]} "
+        f"(mask: {self.mask[ijet]} -> {self.mask[self.nn[ijet]] if self.nn[ijet] >= 0 else None})")
     
